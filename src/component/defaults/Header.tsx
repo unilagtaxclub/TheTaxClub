ada4d34ea3f1bfae6fcb0cb03c93ecda753a0bde
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-04-02 02:54:15 +0100

    designed publication dropdown

--- src/component/defaults/Header.tsx
+++ src/component/defaults/Header.tsx
@@ -7,16 +7,38 @@ import { Logo } from "../svgs/Logos";
 
 const Header = () => {
   const [showDrawer, setShowDrawer] = useState<boolean>(false);
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
 
   return (
     <div>
       <div className="lg:w-[80vw] w-[90vw] mx-auto bg-[#fff] py-2 px-6 rounded-2xl text-[18px] flex justify-between items-center">
-        <NavLink to="/"><Logo /></NavLink>
+        <NavLink to="/">
+          <Logo />
+        </NavLink>
         <div className="space-x-10 lg:flex hidden">
           <NavLink to="/">Home</NavLink>
-          <div className="space-x-3 flex items-center">
-            <span>Publication</span>
-            <CaretDown />
+          <div className="relative">
+            <div
+              className="space-x-3 flex items-center cursor-pointer"
+              onClick={() => setShowDropdown((prev) => !prev)}
+            >
+              <span>Publication</span>
+              <CaretDown />
+            </div>
+            <AnimatePresence>
+              {showDropdown && (
+                <motion.div
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute bg-[#fff] rounded-xl shadow-lg mt-2 flex flex-col p-2 space-y-2 w-[15vw] top-12"
+                >
+                  <span className="text-[15px] hover:bg-[#f1f1f1] hover:rounded-xl p-2 transition-all duration-300 ease-in-out">Featured</span>
+                  <span className="text-[15px] hover:bg-[#f1f1f1] hover:rounded-xl p-2 transition-all duration-300 ease-in-out">Anthology</span>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
           <NavLink to="/about">About</NavLink>
           <span>Our People</span>
@@ -45,7 +67,7 @@ const Header = () => {
               </motion.div>
             ) : (
               <motion.div
-                key="hamburger"                                                                                                                                                                                                                                                                                                                                                                                                                 
+                key="hamburger"
                 initial={{ opacity: 0, scale: 0.8 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.8 }}
