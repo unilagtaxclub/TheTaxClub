7bd7a367f437e9c8346f4dc9b58073995aedf131
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-06-15 05:48:04 +0100

    modified people page

--- src/component/cards/LeadershipCard.tsx
+++ src/component/cards/LeadershipCard.tsx
@@ -10,16 +10,8 @@ const LeadershipCard: FC<LeadershipCardProps> = ({ imgSrc }) => {
       <img
         src={imgSrc}
         alt="community-img"
-        className="w-[100%] h-[80%] object-cover"
+        className="w-[100%] h-[100%] object-cover"
       />
-      <div className="bg-[#0F1A20] p-4 pb-10">
-        <h3 className="lg:text-[20px] uppercase text-[#fff] text-[18px]">
-          Sam Smith
-        </h3>
-        <p className="lg:text-[16px] text-[14px] text-[#fff]">
-          CEO, Company Name
-        </p>
-      </div>
     </div>
   );
 };
--- src/pages/people/index.tsx
+++ src/pages/people/index.tsx
@@ -1,13 +1,102 @@
-import LeadershipCard from "../../component/cards/LeadershipCard";
+import FlippableCard from "../../component/cards/FlippableCard";
 import Container from "../../component/defaults/Container";
 import Footer from "../../component/defaults/Footer";
 import Header from "../../component/defaults/Header";
 
 const OurPeople = () => {
+  const executives = [
+    {
+      id: 1,
+      frontCard: "/assets/people-imgs/jesutofunmi-front.png",
+      backCard: "/assets/people-imgs/jesutofunmi-back.png",
+    },
+    {
+      id: 2,
+      frontCard: "/assets/people-imgs/emmanuel-front.png",
+      backCard: "/assets/people-imgs/emmanuel-back.png",
+    },
+    {
+      id: 3,
+      frontCard: "/assets/people-imgs/jedidiah-front.png",
+      backCard: "/assets/people-imgs/jedidiah-back.png",
+    },
+    {
+      id: 4,
+      frontCard: "/assets/people-imgs/paul-front.png",
+      backCard: "/assets/people-imgs/paul-back.png",
+    },
+    {
+      id: 5,
+      frontCard: "/assets/people-imgs/michael-front.png",
+      backCard: "/assets/people-imgs/michael-back.png",
+    },
+    {
+      id: 6,
+      frontCard: "/assets/people-imgs/taiwo-front.png",
+      backCard: "/assets/people-imgs/taiwo-back.png",
+    },
+    {
+      id: 7,
+      frontCard: "/assets/people-imgs/oluwafeyikemi-front.png",
+      backCard: "/assets/people-imgs/oluwafeyikemi-back.png",
+    },
+    {
+      id: 8,
+      frontCard: "/assets/people-imgs/elum-front.png",
+      backCard: "/assets/people-imgs/elum-back.png",
+    },
+    {
+      id: 9,
+      frontCard: "/assets/people-imgs/kehinde-front.png",
+      backCard: "/assets/people-imgs/kehinde-back.png",
+    },
+  ];
+
+  const staffAdvisers = [
+    {
+      id: 10,
+      frontCard: "/assets/people-imgs/sanni-front.png",
+      backCard: "/assets/people-imgs/sanni-back.png",
+    },
+    {
+      id: 11,
+      frontCard: "/assets/people-imgs/emmanuel-front.png",
+      backCard: "/assets/people-imgs/emmanuel-back.png",
+    },
+    {
+      id: 12,
+      frontCard: "/assets/people-imgs/jesutofunmi-front.png",
+      backCard: "/assets/people-imgs/jesutofunmi-back.png",
+    },
+  ];
+
+  const elders = [
+    {
+      id: 13,
+      frontCard: "/assets/people-imgs/zacch-front.png",
+      backCard: "/assets/people-imgs/zacch-back.png",
+    },
+    {
+      id: 14,
+      frontCard: "/assets/people-imgs/ayodele-front.png",
+      backCard: "/assets/people-imgs/ayodele-back.png",
+    },
+    {
+      id: 15,
+      frontCard: "/assets/people-imgs/jesutofunmi-front.png",
+      backCard: "/assets/people-imgs/jesutofunmi-back.png",
+    },
+    {
+      id: 15,
+      frontCard: "/assets/people-imgs/oyedele-front.png",
+      backCard: "/assets/people-imgs/oyedele-back.png",
+    },
+  ];
+
   return (
     <Container>
       <div className="bg-[#f2f2f2] min-h-screen">
-        <div className="bg-[url('/assets/footer-img.png')] lg:h-[581px] bg-cover bg-no-repeat">
+        <div className="bg-[url('/assets/gallery/ntdv/img-fourteen.png')] lg:h-[581px] bg-cover bg-no-repeat">
           <div className="bg-[rgba(0,0,0,0.6)] text-[#fff] w-[100%] flex flex-col pt-6 lg:pt-20 lg:h-[581px] pb-20 lg:pb-0 lg:px-6">
             <Header active="Our People" />
             <div className="lg:w-[80vw] w-[90vw] mx-auto mt-10 lg:mt-[30vh]">
@@ -25,12 +114,14 @@ const OurPeople = () => {
               </h2>
             </div>
             <div className="mt-6 grid lg:grid-cols-3 grid-cols-1 gap-10">
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-two.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-three.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-four.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-five.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-six.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-seven.jpeg" />
+              {executives.map((executive) => (
+                <div key={executive.id} className="lg:h-[509px] h-[450px]">
+                  <FlippableCard
+                    frontCard={executive.frontCard}
+                    backCard={executive.backCard}
+                  />
+                </div>
+              ))}
             </div>
           </div>
 
@@ -41,9 +132,14 @@ const OurPeople = () => {
               </h2>
             </div>
             <div className="mt-6 grid lg:grid-cols-3 grid-cols-1 gap-10">
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-two.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-three.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-four.jpeg" />
+              {staffAdvisers.map((staff) => (
+                <div key={staff.id} className="lg:h-[509px] h-[450px]">
+                  <FlippableCard
+                    frontCard={staff.frontCard}
+                    backCard={staff.backCard}
+                  />
+                </div>
+              ))}
             </div>
           </div>
 
@@ -54,10 +150,14 @@ const OurPeople = () => {
               </h2>
             </div>
             <div className="mt-6 grid lg:grid-cols-3 grid-cols-1 gap-10">
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-two.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-three.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-four.jpeg" />
-              <LeadershipCard imgSrc="/assets/community-imgs/community-img-five.jpeg" />
+              {elders.map((elder) => (
+                <div key={elder.id} className="lg:h-[509px] h-[450px]">
+                  <FlippableCard
+                    frontCard={elder.frontCard}
+                    backCard={elder.backCard}
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
