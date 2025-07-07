2be5dac93e9dbdb5ab2c3489be2d3b5302f120fc
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-07-07 04:37:49 +0100

    edited feedback page

--- src/component/sections/Feedback.tsx
+++ src/component/sections/Feedback.tsx
@@ -7,35 +7,18 @@ const images = [
     caption:
       "“The Tax Club is made up of the best minds and is an avenue to not only broaden gain deep insights into taxation but also spread your wings and fly both on national and an international stages”",
     author: "Mazi Afam Osigwe",
-    title: "President, Nigerian Bar Association",
   },
   {
     src: "/assets/feedback-imgs/img-two.png",
     caption:
-      "“The Tax Club is made up of the best minds and is an avenue to not only broaden gain deep insights into taxation but also spread your wings and fly both on national and an international stages”",
-    author: "Mazi Afam Osigwe",
-    title: "President, Nigerian Bar Association",
+      "“Becoming a member of the tax club, Unilag gave me the exposure i neened to not only learn and understand Tax, but also to make valuable contributions to the development of Taxation in the country.” I would definitely recommend joining the Tax Club to anybody who cares to listen",
+    author: "Prince Obiora",
   },
   {
     src: "/assets/feedback-imgs/img-three.png",
     caption:
-      "“The Tax Club is made up of the best minds and is an avenue to not only broaden gain deep insights into taxation but also spread your wings and fly both on national and an international stages”",
-    author: "Mazi Afam Osigwe",
-    title: "President, Nigerian Bar Association",
-  },
-  {
-    src: "/assets/feedback-imgs/img-four.png",
-    caption:
-      "“The Tax Club is made up of the best minds and is an avenue to not only broaden gain deep insights into taxation but also spread your wings and fly both on national and an international stages”",
-    author: "Mazi Afam Osigwe",
-    title: "President, Nigerian Bar Association",
-  },
-  {
-    src: "/assets/feedback-imgs/img-five.png",
-    caption:
-      "“The Tax Club is made up of the best minds and is an avenue to not only broaden gain deep insights into taxation but also spread your wings and fly both on national and an international stages”",
-    author: "Mazi Afam Osigwe",
-    title: "President, Nigerian Bar Association",
+      "“Founding the Tax Club, University of Lagos, was a defining moment in my academic and professional journey. It laid the foundation for impactful tax education, created a platform for future leaders, and showed how student-driven initiatives can influence national policy conversations. I am proud to see the legacy thrive across Nigeria today.",
+    author: "ThankGod Enahoro",
   },
 ];
 
@@ -66,7 +49,7 @@ const Feedback = () => {
         What people say about us
       </p>
 
-      <div className="flex justify-center items-center w-full lg:space-x-4 px-4 lg:px-10">
+      <div className="flex justify-center lg:items-center w-full lg:space-x-4 px-4 lg:px-10">
         <motion.div
           key={getImage(-1).src}
           className="hidden lg:block w-[580px] h-[300px] rounded-xl -mt-[23vh]"
@@ -81,11 +64,11 @@ const Feedback = () => {
           />
         </motion.div>
 
-        <div className="flex flex-col items-center space-y-6 relative max-w-[90vw] lg:max-w-[600px]">
+        <div className="flex flex-col items-center space-y-6 relative max-w-[60vw] lg:max-w-[600px]">
           <AnimatePresence mode="wait">
             <motion.div
               key={getImage(0).src}
-              className="w-full h-[260px] lg:h-[360px] rounded-xl overflow-hidden shadow-xl"
+              className="h-[260px] lg:w-full w-[200px] lg:h-[360px] rounded-xl overflow-hidden shadow-xl"
               initial={{ opacity: 0, scale: 0.95 }}
               animate={{ opacity: 1, scale: 1.02 }}
               exit={{ opacity: 0, scale: 0.95 }}
@@ -99,22 +82,19 @@ const Feedback = () => {
             </motion.div>
           </AnimatePresence>
 
-          <div className="text-center px-4">
+          <div className="text-center lg:block hidden px-4">
             <p className="text-[16px] lg:text-[20px] text-gray-700 mb-4">
-              {getImage(0).caption}
+              {getImage(centerIndex).caption}
             </p>
             <h2 className="text-[#00689E] text-[20px] lg:text-[24px] font-semibold">
-              {getImage(0).author}
+              {getImage(centerIndex).author}
             </h2>
-            <p className="text-[14px] lg:text-[16px] text-[#808080]">
-              {getImage(0).title}
-            </p>
           </div>
         </div>
 
         <motion.div
           key={getImage(1).src}
-          className="hidden lg:block w-[580px] h-[300px] rounded-xl overflow-hidden -mt-[23vh]"
+          className="lg:w-[580px] lg:h-[300px] h-[180px] rounded-xl overflow-hidden lg:-mt-[23vh] mt-10"
           initial={{ opacity: 0, rotateY: 45, scale: 0.9 }}
           animate={{ opacity: 1, rotateY: 25, scale: 0.95 }}
           transition={{ duration: 0.6 }}
@@ -125,6 +105,14 @@ const Feedback = () => {
             className="w-full h-full object-cover rounded-xl"
           />
         </motion.div>
+      </div>
+      <div className="text-center block lg:hidden px-4">
+        <p className="text-[16px] lg:text-[20px] text-gray-700 mb-4">
+          {getImage(centerIndex).caption}
+        </p>
+        <h2 className="text-[#00689E] text-[20px] lg:text-[24px] font-semibold">
+          {getImage(centerIndex).author}
+        </h2>
       </div>
     </div>
   );
