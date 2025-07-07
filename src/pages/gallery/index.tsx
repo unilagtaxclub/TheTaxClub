f72405b491b4b2884c8aa7e7bc7aed4e72fe4bd1
Frokesy <akindeleayanfeoluwa@gmail.com>
2025-07-07 01:52:35 +0100

    modified gallery page

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -10,7 +10,7 @@ const Gallery = () => {
   return (
     <Container>
       <div className="bg-[#f2f2f2] min-h-screen ">
-        <div className="bg-[url('/assets/footer-img.png')] lg:h-[581px] bg-cover bg-no-repeat">
+        <div className="bg-[url('/assets/hero-imgs/gallery-hero-img.png')] lg:h-[581px] bg-cover bg-no-repeat">
           <div className="bg-[rgba(0,0,0,0.6)] text-[#fff] w-[100%] flex flex-col pt-6 lg:pt-20 lg:h-[581px] pb-10 lg:pb-0 lg:px-6">
             <Header active="Gallery" color="#ffffff" />
             <div className="lg:w-[80vw] w-[90vw] mx-auto mt-10 lg:mt-[30vh]">
@@ -31,27 +31,18 @@ const Gallery = () => {
           </p>
 
           <div className="mt-10">
-            <div className="flex justify-between items-center mb-10">
-              <div className="">
-                <h2 className="lg:text-[32px] text-[20px] font-semibold">
-                  NTDV Publicity Shot
-                </h2>
-                <div className="flex items-center space-x-2">
-                  <MiniCalendarIcon />
-                  <p className="lg:text-[16px] text-[14px]">
-                    30th February 2023
-                  </p>
-                </div>
-              </div>
-              <NavLink
-                to="#"
-                className="lg:text-[20px] underline text-[#00689e] font-semibold"
-              >
-                See all
-              </NavLink>
+            <NavLink
+              to="#"
+              className="lg:text-[32px] text-[20px] font-semibold"
+            >
+              NTD 8.0
+            </NavLink>
+            <div className="flex items-center space-x-2">
+              <MiniCalendarIcon />
+              <p className="lg:text-[16px] text-[14px]">30th February 2023</p>
             </div>
 
-            <div className="space-y-6">
+            <div className="space-y-6 mt-10">
               <LayoutOne
                 imgOne="/assets/gallery/ntdv-two/one.png"
                 imgTwo="/assets/gallery/ntdv-two/two.png"
@@ -75,26 +66,19 @@ const Gallery = () => {
           </div>
 
           <div className="mt-10">
-            <div className="flex justify-between items-center mb-10">
-              <div className="">
-                <h2 className="lg:text-[32px] text-[20px] font-semibold">
-                  NTDV Publicity Shot
-                </h2>
-                <div className="flex items-center space-x-2">
-                  <MiniCalendarIcon />
-                  <p className="lg:text-[16px] text-[14px]">4 Jul 2023</p>
-                </div>
-              </div>
-              <NavLink
-                to="https://drive.google.com/drive/folders/1KBgkL7SlddepbsKrlZTRrOyh4XH6GFXK"
-                target="_blank"
-                className="lg:text-[20px] underline text-[#00689e] font-semibold"
-              >
-                See all
-              </NavLink>
+            <NavLink
+              to="https://drive.google.com/drive/folders/1KBgkL7SlddepbsKrlZTRrOyh4XH6GFXK"
+              target="_blank"
+              className="lg:text-[32px] text-[20px] font-semibold"
+            >
+              NTDV Publicity Shot
+            </NavLink>
+            <div className="flex items-center space-x-2">
+              <MiniCalendarIcon />
+              <p className="lg:text-[16px] text-[14px]">4 Jul 2023</p>
             </div>
 
-            <div className="space-y-6">
+            <div className="space-y-6 mt-10">
               <LayoutOne
                 imgOne="/assets/gallery/ntdv/img-one.png"
                 imgTwo="/assets/gallery/ntdv/img-two.png"
@@ -118,28 +102,19 @@ const Gallery = () => {
           </div>
 
           <div className="mt-20">
-            <div className="flex justify-between items-center mb-10">
-              <div className="">
-                <h2 className="lg:text-[32px] text-[20px] font-semibold">
-                  Tax Debate 6.0
-                </h2>
-                <div className="flex items-center space-x-2">
-                  <MiniCalendarIcon />
-                  <p className="lg:text-[16px] text-[14px]">
-                    30th January, 2023
-                  </p>
-                </div>
-              </div>
-              <NavLink
-                to="https://drive.google.com/drive/u/0/folders/15nt9cA6BpE7m58oW7wAaYJ4UUKgKMV2Z"
-                target="_blank"
-                className="lg:text-[20px] underline text-[#00689e] font-semibold"
-              >
-                See all
-              </NavLink>
+            <NavLink
+              to="https://drive.google.com/drive/u/0/folders/15nt9cA6BpE7m58oW7wAaYJ4UUKgKMV2Z"
+              target="_blank"
+              className="lg:text-[32px] text-[20px] font-semibold"
+            >
+              National Tax Debate 7.0
+            </NavLink>
+            <div className="flex items-center space-x-2">
+              <MiniCalendarIcon />
+              <p className="lg:text-[16px] text-[14px]">30th January, 2023</p>
             </div>
 
-            <div className="space-y-6">
+            <div className="space-y-6 mt-10">
               <LayoutTwo
                 imgOne="/assets/gallery/ntdv-two/fifteen.png"
                 imgTwo="/assets/gallery/ntdv-two/sixteen.png"
